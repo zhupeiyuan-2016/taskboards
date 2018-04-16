eebd4950f86a5c1d24e879b8d50fd603d192e859
jiumi <jiumilove@163.com>
2018-04-16 21:30:24 +0800

    localStorage curl“

--- localstorage.js
+++ localstorage.js
@@ -1,24 +1,58 @@
-
 function curl(data) {
-    console.log("欢迎")
+    console.log("欢迎");
 };
-curl.prototype.PushToDoing = function (e) {
-    const ToDoing = curl.GetToDoing;
-    localStorage.setItem('准备做', JSON.stringify(ToDoing))
-};
-curl.prototype.PushDoing = function (e) {
-    this.Doing.push(e);
-    localStorage.setItem('正在做', JSON.stringify(this.Doing));
-}
-curl.prototype.PushEd = function (e) {
-    this.Ed = e;
-    localStorage.setItem('已经做', JSON.stringify(this.Ed));
-}
+//JSON.stringify 把一个对象转换成json字符串， 
+ //JSON.parse 把一个json字符串解析成对象
 curl.prototype.GetToDoing = function () {
-    const ToDoing = JSON.parse(localStorage.getItem('准备做'));
+    console.log('获取讲要做的事情');
+    const ToDoing = JSON.parse(localStorage.getItem("ToDoing"));
+    console.log(ToDoing)
     return ToDoing;
 }
+curl.prototype.GetDoing = function () {
+    console.log('获取正在做的事情');
+    const Doing = JSON.parse(localStorage.getItem("Doing"));
+    return Doing;
+}
+curl.prototype.GetEd = function () {
+    console.log('获取已经完成的事情');
+    const Ed = JSON.parse(localStorage.getItem("Ed"));
+    return Ed;
+}
+curl.prototype.PushToDoing = function (e) {
+    let data = [];
+    ToDoing = this.GetToDoing();
+    if (!ToDoing) {
+        data[0] = e;
+        localStorage.setItem("ToDoing",JSON.stringify(data));
+        return 0;
+    };
+    ToDoing.push(e);
+    localStorage.setItem("ToDoing",JSON.stringify(ToDoing));
+};
+curl.prototype.PushDoing = function (e) {
+    let data = [];
+    Doing = this.GetDoing();
+    if (!Doing) {
+        data[0] = e;
+        localStorage.setItem("Doing",JSON.stringify(data));
+        return 0;
+    };
+    Doing.push(e);
+    localStorage.setItem("Doing",JSON.stringify(Doing));
+}
+curl.prototype.PushEd = function (e) {
+    let data = [];
+    Ed = this.GetEd();
+    if (!Ed) {
+        data[0] = e;
+        localStorage.setItem("Ed",JSON.stringify(data));
+        return 0;
+    };
+    Ed.push(e);
+    localStorage.setItem("Ed",JSON.stringify(Ed));
+}
 curl.prototype.Clear = function (e) {
+    console.log('清除全部')
     localStorage.clear();
 };
-
